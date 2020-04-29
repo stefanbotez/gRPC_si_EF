c0d475c9abaa5fbc4defd4df5db9bd3880d00a07
Stefan Botez <stfn_botez@yahoo.com>
2020-04-29 13:46:19 +0300

    INITIAL

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GrpcService;
+using Grpc.Net.Client;
+using Grpc.Core.Interceptors;
+using Google.Protobuf.WellKnownTypes;
+using System.Threading;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClientHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            httpClientHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var httpClient = new HttpClient(httpClientHandler);
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001",
+                new GrpcChannelOptions { HttpClient = httpClient });
+            var PC = new PostSv.PostSvClient(channel);
+            var result = await PC.AddPostAsync(
+                new PostModel
+                {
+                    Id = 9,
+                    Description = "DESCRIPTIONX",
+                    Domain = "DOMAINX",
+                    Date = Timestamp.FromDateTime(DateTime.UtcNow)
+                });
+            Console.WriteLine("Result: " + result.Message);
+            var result2 = await PC.AddPostAsync(
+            new PostModel
+            {
+                Id = 14,
+                Description = "DESCRIPTIONXX",
+                Domain = "DOMAINXX",
+                Date = Timestamp.FromDateTime(DateTime.UtcNow)
+            });
+            Console.WriteLine("Result: " + result2.Message);
+
+            using (var call = PC.GetAllPosts(new GetAllPostsRequest()))
+            {
+                while(await call.ResponseStream.MoveNext(CancellationToken.None))
+                {
+                    var currentPost = call.ResponseStream.Current;
+                    Console.WriteLine($"{currentPost.Domain} {currentPost.Description}");
+                }
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcService/Services/PostService.cs
@@ -0,0 +1,89 @@
+﻿using GrpcService;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PostComment.APIStatic;
+using Grpc.Core;
+using PostComment;
+using Google.Protobuf.WellKnownTypes;
+
+namespace GrpcService
+{
+    public class PostService : PostSv.PostSvBase
+    {
+        private readonly ILogger<PostService> _logger;
+
+        public PostService(ILogger<PostService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<PostModel> GetPost(PostIdModel request, ServerCallContext context)
+        {
+            Post post = API.GetPostById(request.Id);
+
+            return Task.FromResult(new PostModel
+            {
+                Id = post.PostId,
+                Description = post.Description,
+                Domain = post.Domain,
+                Date = Timestamp.FromDateTime(post.Date)
+            });
+        }
+
+        public override Task<PostResult> AddPost(PostModel request, ServerCallContext context)
+        {
+            Post post = new Post();
+            post.Description = request.Description;
+            post.Domain = request.Domain;
+            post.Date = request.Date.ToDateTime();
+            var result = API.AddPost(post);
+            if (result)
+                return Task.FromResult(new PostResult { Message = "GOOD" });
+            else
+                return Task.FromResult(new PostResult { Message = "BAD" });
+        }
+
+        public override Task<PostResult> UpdatePost(PostModel request, ServerCallContext context)
+        {
+            Post post = new Post();
+            post.Description = request.Description;
+            post.Domain = request.Domain;
+            post.Date = request.Date.ToDateTime();
+            var result = API.UpdatePost(post);
+            if (result != null)
+                return Task.FromResult(new PostResult { Message = "GOOD" });
+            else
+                return Task.FromResult(new PostResult { Message = "BAD" });
+        }
+
+        public override Task<PostResult> DeletePost(PostIdModel request, ServerCallContext context)
+        {
+            var result = API.DeletePost(request.Id);
+            if (result != 0)
+                return Task.FromResult(new PostResult { Message = "GOOD" });
+            else
+                return Task.FromResult(new PostResult { Message = "BAD" });
+        }
+
+        public override async Task GetAllPosts(
+            GetAllPostsRequest request, 
+            IServerStreamWriter<PostModel> responseStream, 
+            ServerCallContext context)
+        {
+            foreach(var post in API.GetAllPosts())
+            {
+                PostModel pm = new PostModel
+                {
+                    Id = post.PostId,
+                    Description = post.Description,
+                    Domain = post.Domain,
+                    Date = Timestamp.FromDateTime(post.Date)
+                };
+                await responseStream.WriteAsync(pm);
+            }
+        }
+    }
+}
